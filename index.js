26ecc286bdf12bfd48cb8e18d7617271a3abb179
Zeeshan Darediya <zeeshan@mapout.com>
2022-06-06 07:48:38 +0000

    changed port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const mongoose = require('mongoose')
 
-const port = process.env.PORT || 4040;
+const port = process.env.PORT || 4041;
 
 app.listen(port, () => {
-  console.log("App running on http://localhost:4040");
+  console.log("App running on http://localhost:4041");
 });
