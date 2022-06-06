5fb8048202980d525065909f1a64c85d2b5d5c7f
Zeeshan Darediya <zeeshan@mapout.com>
2022-06-06 07:13:48 +0000

    updated mongodb string uri

--- src/controllers/professiontoskill/professiontoskill.js
+++ src/controllers/professiontoskill/professiontoskill.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router()
 const mongoose = require('mongoose');
 //const mongoString = process.env.DATABASE_URL;
-const mongoString = "mongodb+srv://mapout:mapout@mapoutdb.hj2on.mongodb.net/mapout-test?authSource=admin&replicaSet=atlas-1389yt-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+const mongoString = "mongodb+srv://mapout:mapout@mapoutdb.hj2on.mongodb.net/mapout-staging?authSource=admin&replicaSet=atlas-1389yt-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
