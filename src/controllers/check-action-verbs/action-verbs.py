3707bb62ea993da5e68d76b1a4930a4c1d5498ad
Zeeshan Darediya <zeeshan@mapout.com>
2022-10-17 11:21:54 +0530

    optimized from 800ms to 300ms

--- src/controllers/check-action-verbs/action-verbs.py
+++ src/controllers/check-action-verbs/action-verbs.py
@@ -1,31 +1,15 @@
-import pandas as pd
 import sys
-from nltk.corpus import stopwords
-import spacy
 
-# FIRST TIME INSTALLATION
-#import nltk
-#nltk.download('stopwords')
-
-stopwords = set(stopwords.words('english'))
-
-
-from spacy.lang.en import English
-
-nlp=English()
-
-# load pre-trained model
-nlp = spacy.load('en_core_web_sm')
-
-def action_words(nlp_text):
-  
-    # removing stop words and implementing word tokenization
-    tokens = [token.text for token in nlp_text if not token.is_stop]
-    flag = 0
-    
+def get_action_verbs_df():
+    import pandas as pd
     df = pd.read_csv('src/controllers/check-action-verbs/all_action_verbs.csv')   
-    words = list(df['0'].values)
-  
+    words = list(df['action_verb'].values)
+    return words
+    
+def action_words(text):
+    tokens = text.split(" ")
+    flag = 0
+    words = get_action_verbs_df()
     #action_words = []
     
     for token in tokens:
@@ -34,12 +18,10 @@ def action_words(nlp_text):
         if token.lower() in words:
             #action_words.append(token.lower())
             flag = 1
-    
+
     return flag #, action_words
 
 text = sys.argv[1]
 #text = " Implemented xyz xyz xyz abc abc abc and performed xyz xyz xyz abc abc abc "
-
-nlptext = nlp(text)
-print(action_words(nlptext))
+print(action_words(text))
 
